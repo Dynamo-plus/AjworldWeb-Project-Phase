8dce796594d3207889f9e8b4810a02e91fe448f4
Dynamo-plus <88222665+Dynamo-plus@users.noreply.github.com>
2023-01-18 12:22:10 -0800

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require "config/database.php";
+if(!isset($_SESSION['user-id'])){
+    header('location: index.php');
+    die();
+}else{
+    $id = filter_var($_SESSION['user-id'], FILTER_SANITIZE_NUMBER_INT);
+
+    $get_id_query = "SELECT * FROM users WHERE id = '$id'";
+    $get_id_result = mysqli_query($connection, $get_id_query);
+
+    if($get_id_result){
+        $data = mysqli_fetch_assoc($get_id_result);
+    }
+}    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/homepage-style.css">
+    <title>Welcome | AjworldWeb</title>
+</head>
+<body>
+    <nav>
+        <a href="logout.php">
+            <img src="assets/icon/icon-logout.svg" alt="">
+            <h4>Log Out</h4>
+        </a>
+    </nav>
+
+    <div class="user-details">
+        <h1>Hi, <?= ucfirst($data['username']); ?> </h1>
+        <span>Email: </span> <p><?= $data['email'] ?></p>
+    </div>
+    <div class="homepage-content">
+        <h1>Welcome to Homepage</h1>
+    </div>
+</body>
+</html>
